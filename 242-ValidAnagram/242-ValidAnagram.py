162c3f3c6e7c5488f79e129e25afac33d3c22fa1
Anish Gada <92446208+Anish478@users.noreply.github.com>
2025-07-15 17:50:41 -0400

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,21 @@
+# Last updated: 7/15/2025, 5:50:41 PM
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = sorted(s)
+        t = sorted(t)
+
+        if s == t:
+            return True
+        else:
+            return False
+
+
+
+        
+
+
+            
+            
+            
+
+        
